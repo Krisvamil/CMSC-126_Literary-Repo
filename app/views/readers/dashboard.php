9e4f800b214f139247b0d10e6a3c50170a6eeec4
Krisvamil <kristjanivanmickaeldiva@gmail.com>
2025-05-13 20:56:06 +0800

    Update dashboard.php

--- app/views/readers/dashboard.php
+++ app/views/readers/dashboard.php
@@ -1,5 +1,9 @@
 <?php
-$scripts = ['/js/readers.js'];
+$title       = 'Reader Dashboard';
+$pageStyles  = ['readers.css'];
+$pageScripts = ['readers.js'];
+
+// Buffer the page-specific HTML
 ob_start();
 ?>
 <h2>Reader Dashboard</h2>
@@ -15,11 +19,17 @@ ob_start();
     <h3>Following</h3>
     <ul class="author-list">
         <?php foreach ($following as $author): ?>
-            <li><a href="<?= url('/authors/' . $author['id']) ?>"><?= htmlspecialchars($author['name']) ?></a></li>
+            <li>
+                <a href="<?= url('/authors/' . $author['id']) ?>">
+                    <?= htmlspecialchars($author['name'], ENT_QUOTES) ?>
+                </a>
+            </li>
         <?php endforeach; ?>
     </ul>
 </section>
 <?php
+// Get the buffered content
 $content = ob_get_clean();
-$title = 'Reader Dashboard';
+
+// Delegate rendering (and CSP nonce injection) to the main layout
 require __DIR__ . '/../../layout.php';
