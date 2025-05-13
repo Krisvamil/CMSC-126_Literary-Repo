0ce89e69131f9cd298e436f53e918a5f73e5458e
Krisvamil <kristjanivanmickaeldiva@gmail.com>
2025-05-13 20:57:43 +0800

    Update _flash.php

--- app/views/partials/_flash.php
+++ app/views/partials/_flash.php
@@ -1,9 +1,10 @@
 <?php if (!empty($_SESSION['flash'])): ?>
   <?php foreach ($_SESSION['flash'] as $type => $message): ?>
-    <div class="flash <?= $type ?>">
-      <?= htmlspecialchars($message) ?>
+    <div class="flash <?= htmlspecialchars($type, ENT_QUOTES) ?>">
+      <?= htmlspecialchars($message, ENT_QUOTES) ?>
     </div>
   <?php endforeach;
     unset($_SESSION['flash']);
   ?>
 <?php endif; ?>
+
