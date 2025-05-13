b6b71f4f2c03f422c275672df69b21d971f2695d
Krisvamil <kristjanivanmickaeldiva@gmail.com>
2025-05-13 20:58:13 +0800

    Update _header.php

--- app/views/partials/_header.php
+++ app/views/partials/_header.php
@@ -1,5 +1,9 @@
 <header class="site-header">
   <div class="container d-flex justify-between align-center">
-    <h1><a href="<?= BASE_URL ?>/">Literary Repo</a></h1>
+    <h1>
+      <a href="<?= htmlspecialchars(BASE_URL, ENT_QUOTES) ?>/">
+        <?= htmlspecialchars('Literary Repo', ENT_QUOTES) ?>
+      </a>
+    </h1>
   </div>
 </header>
